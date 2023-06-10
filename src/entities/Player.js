ba0a4c90d4d9975eea0de9de690f198f4ae0d6db
Alex Roche <aroche42@gmail.com>
2023-06-10 16:26:04 -0700

    clean draw function

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -11,54 +11,61 @@ export default class Player extends Ship {
         this.graphics = this.scene.add.graphics({ lineStyle: { color: 0xffffff } });  // White color
 
         this.moveState = 'stop';
-
+        this.triangleLength = 10;
         
         this.approachText = this.scene.add.text(0, 0, 'Approach', { color: '#ffffff', fontSize: '12px' }).setOrigin(0.5, 0.5);
         this.approachText.setVisible(false);
     }
 
+    drawApproachLine(fromX, fromY, toX, toY) {
+        this.graphics.beginPath();
+        let lineX = fromX + 2 * this.triangleLength * Math.cos(Math.atan2(toY - fromY, toX - fromX));
+        let lineY = fromY + 2 * this.triangleLength * Math.sin(Math.atan2(toY - fromY, toX - fromX));
+        this.graphics.moveTo(lineX, lineY);
+        this.graphics.lineTo(toX, toY);
+        this.graphics.strokePath();
+        this.graphics.closePath();
+
+    }
+    drawApproachText(fromX, fromY, toX, toY) {
+        // Write the distance from the ship to the approach point at the end of the line
+        let distance = Phaser.Math.Distance.Between(this.position.x, this.position.y, this.approach.x, this.approach.y);
+        let distanceText = (distance / 1000).toFixed(1) + ' km';
+        // Offset the text from the end of the line
+        let textX = toX + 5 * distanceText.length * Math.cos(Math.atan2(toY - fromY, toX - fromX));
+        let textY = toY + 10 * Math.sin(Math.atan2(toY - fromY, toX - fromX));
+        this.approachText.setPosition(textX, textY);
+        this.approachText.setText(distanceText);
+        this.approachText.setVisible(true);
+    }
+    drawShipTriangle(x, y) {
+        // Draw the ship as a triangle at the ship's position
+        this.graphics.beginPath();
+        this.graphics.moveTo(x, y - this.triangleLength);
+        this.graphics.lineTo(x + this.triangleLength, y + this.triangleLength);
+        this.graphics.lineTo(x - this.triangleLength, y + this.triangleLength);
+        this.graphics.lineTo(x, y - this.triangleLength);
+        this.graphics.strokePath();
+        this.graphics.closePath();
+    }
+
     draw() {
-        const triangleLength = 10;
 
         // Clear the previous frame's graphics
         this.graphics.clear();
 
         let screenCoordinates = this.screenToWorld.toScreenCoordinates(this.position.x, this.position.y);
 
-        // Write to console the player's position in screen coordinates
-        // console.log(screenCoordinates);
-
-        // Draw the ship as a triangle at the ship's position
-        this.graphics.beginPath();
-        this.graphics.moveTo(screenCoordinates.x, screenCoordinates.y - triangleLength);
-        this.graphics.lineTo(screenCoordinates.x + triangleLength, screenCoordinates.y + triangleLength);
-        this.graphics.lineTo(screenCoordinates.x - triangleLength, screenCoordinates.y + triangleLength);
-        this.graphics.lineTo(screenCoordinates.x, screenCoordinates.y - triangleLength);
-        this.graphics.strokePath();
-        this.graphics.closePath();
+        this.drawShipTriangle(screenCoordinates.x, screenCoordinates.y);
 
         // If the moveState is 'approach', draw a line from the ship to the approach point.
         // The start of the line should be offset from the ship
         if (this.moveState === 'approach') {
             let approachScreenCoords = this.screenToWorld.toScreenCoordinates(this.approach.x, this.approach.y);
-            this.graphics.beginPath();
-            let lineX = screenCoordinates.x + 2 * triangleLength * Math.cos(Math.atan2(approachScreenCoords.y - screenCoordinates.y, approachScreenCoords.x - screenCoordinates.x));
-            let lineY = screenCoordinates.y + 2 * triangleLength * Math.sin(Math.atan2(approachScreenCoords.y - screenCoordinates.y, approachScreenCoords.x - screenCoordinates.x));
-            this.graphics.moveTo(lineX, lineY);
-            this.graphics.lineTo(approachScreenCoords.x, approachScreenCoords.y);
-            this.graphics.strokePath();
-            this.graphics.closePath();
 
+            this.drawApproachLine(screenCoordinates.x, screenCoordinates.y, approachScreenCoords.x, approachScreenCoords.y);
+            this.drawApproachText(screenCoordinates.x, screenCoordinates.y, approachScreenCoords.x, approachScreenCoords.y);
 
-            // Write the distance from the ship to the approach point at the end of the line
-            let distance = Phaser.Math.Distance.Between(this.position.x, this.position.y, this.approach.x, this.approach.y);
-            let distanceText = (distance / 1000).toFixed(1) + ' km';
-            // Offset the text from the end of the line
-            let textX = approachScreenCoords.x + 5 * distanceText.length * Math.cos(Math.atan2(approachScreenCoords.y - screenCoordinates.y, approachScreenCoords.x - screenCoordinates.x));
-            let textY = approachScreenCoords.y + 10 * Math.sin(Math.atan2(approachScreenCoords.y - screenCoordinates.y, approachScreenCoords.x - screenCoordinates.x));
-            this.approachText.setPosition(textX, textY);
-            this.approachText.setText(distanceText);
-            this.approachText.setVisible(true);
         }
 
     }
