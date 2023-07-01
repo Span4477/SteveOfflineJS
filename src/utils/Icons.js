ba288f780440a38b1d045e90c9fb9876d22483cb
Alex Roche <aroche42@gmail.com>
2023-06-30 20:23:19 -0700

    icons

---
+++ src/utils/Icons.js
@@ -0,0 +1,59 @@
+export const icons = [
+    //stars
+    {
+        key: 'star1',
+        path: 'assets/icons/celestial/stars/star1.png'
+    },
+    {
+        key: 'star2',
+        path: 'assets/icons/celestial/stars/star2.png'
+    },
+    // gates
+    {
+        key: 'g1',
+        path: 'assets/icons/celestial/gates/g1.png'
+    },
+    {
+        key: 'g2',
+        path: 'assets/icons/celestial/gates/g2.png'
+    },
+    // planets
+    {
+        key: 'p1',
+        path: 'assets/icons/celestial/planets/p1.png'
+    },
+    {
+        key: 'p2',
+        path: 'assets/icons/celestial/planets/p2.png'
+    },
+    {
+        key: 'p3',
+        path: 'assets/icons/celestial/planets/p3.png'
+    },
+    {
+        key: 'p4',
+        path: 'assets/icons/celestial/planets/p4.png'
+    },
+    {
+        key: 'p5',
+        path: 'assets/icons/celestial/planets/p5.png'
+    },
+    // enemy ships
+    {
+        key: 'e1',
+        path: 'assets/icons/ships/e1.png'
+    },
+    {
+        key: 'e2',
+        path: 'assets/icons/ships/e2.png'
+    },
+    {
+        key: 'e3',
+        path: 'assets/icons/ships/e3.png'
+    },
+    // player ships
+    {
+        key: 'ship1',
+        path: 'assets/icons/ships/ship1.png'
+    }
+]
